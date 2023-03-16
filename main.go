fba5dd6f9dc41811c73050ea016badbee1010c36
Doğukan Teber <dogukanteber1@hotmail.com>
2023-03-17 00:32:48 +0300

    Update main.go file
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/cortexproject/auth-gateway/gateway"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	targetURL := "http://localhost:8888"
+	proxy, err := gateway.NewProxy(targetURL)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if len(os.Args) < 2 {
+		log.Println("Please provide a file path as an argument")
+		os.Exit(1)
+	}
+
+	filePath := os.Args[1]
+	gateway.InitTenants(filePath)
+
+	http.Handle("/", gateway.Authenticate(proxy))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
