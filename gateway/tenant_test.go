38aa32ee35d7c4389c1944e3ce2c06e2db11573a
Doğukan Teber <dogukanteber1@hotmail.com>
2023-03-18 17:10:18 +0300

    Add tests for tenant.go file
    Signed-off-by: Doğukan Teber <dogukanteber1@hotmail.com>

---
+++ gateway/tenant_test.go
@@ -0,0 +1,68 @@
+package gateway
+
+import (
+	"errors"
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestGetTenants(t *testing.T) {
+	testCases := []struct {
+		name            string
+		filePath        string
+		expectedTenants []Tenant
+		expectedErr     error
+	}{
+		{
+			name:     "Valid input file",
+			filePath: "testdata/valid.yaml",
+			expectedTenants: []Tenant{
+				{
+					ID:       "orgid1",
+					Username: "username1",
+					Password: "password1",
+				},
+				{
+					ID:       "orgid2",
+					Username: "username2",
+					Password: "password2",
+				},
+			},
+			expectedErr: nil,
+		},
+		{
+			name:            "Invalid input file",
+			filePath:        "testdata/invalid.yaml",
+			expectedTenants: []Tenant{},
+			expectedErr:     errors.New("yaml: unmarshal errors:\n  line 2: cannot unmarshal !!seq into map[string]gateway.Tenant"),
+		},
+		{
+			name:            "Non-existent input file",
+			filePath:        "testdata/nonexistent.yaml",
+			expectedTenants: []Tenant{},
+			expectedErr:     &os.PathError{Op: "open", Path: "testdata/nonexistent.yaml", Err: errors.New("no such file or directory")},
+		},
+		{
+			name:            "Empty input file",
+			filePath:        "testdata/empty.yaml",
+			expectedTenants: []Tenant{},
+			expectedErr:     nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tenants, err := GetTenants(tc.filePath)
+			if !reflect.DeepEqual(tenants, tc.expectedTenants) {
+				t.Errorf("Unexpected result: got %v, want %v", tenants, tc.expectedTenants)
+			}
+			if err == nil && tc.expectedErr != nil || err != nil && tc.expectedErr == nil {
+				t.Errorf("Unexpected error: got %v, want %v", err, tc.expectedErr)
+			}
+			if err != nil && tc.expectedErr != nil && err.Error() != tc.expectedErr.Error() {
+				t.Errorf("Unexpected error: got %v, want %v", err, tc.expectedErr)
+			}
+		})
+	}
+}
